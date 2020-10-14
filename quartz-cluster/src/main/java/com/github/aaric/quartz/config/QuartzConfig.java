20410c796ed8c2db56070f23591714721884e7ee
qlhan <qlhan@incarcloud.com>
2020-10-14 10:13:16 +0800

    0.2.0-SNAPSHOT quartz-cluster

--- quartz-cluster/src/main/java/com/github/aaric/quartz/config/QuartzConfig.java
+++ quartz-cluster/src/main/java/com/github/aaric/quartz/config/QuartzConfig.java
@@ -1,19 +1,14 @@
 package com.github.aaric.quartz.config;
 
+import com.github.aaric.quartz.quartz.CustomJobFactory;
 import org.quartz.Scheduler;
 import org.quartz.ee.servlet.QuartzInitializerListener;
-import org.quartz.spi.TriggerFiredBundle;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.beans.factory.config.PropertiesFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.scheduling.quartz.AdaptableJobFactory;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
-import org.springframework.stereotype.Component;
 
-import javax.sql.DataSource;
 import java.io.IOException;
 import java.util.Properties;
 
@@ -26,29 +21,20 @@ import java.util.Properties;
 @Configuration
 public class QuartzConfig {
 
-    @Autowired
-    private DataSource dataSource;
-
-    @Autowired
-    private QuartzJobFactory quartzJobFactory;
-
     @Bean
     public Properties quartzProperties() throws IOException {
-        PropertiesFactoryBean factoryBean = new PropertiesFactoryBean();
-        factoryBean.setLocation(new ClassPathResource("quartz.properties"));
-        factoryBean.afterPropertiesSet();
-        return factoryBean.getObject();
+        PropertiesFactoryBean propertiesFactoryBean = new PropertiesFactoryBean();
+        propertiesFactoryBean.setLocation(new ClassPathResource("quartz.properties"));
+        propertiesFactoryBean.afterPropertiesSet();
+        return propertiesFactoryBean.getObject();
     }
 
     @Bean
-    public SchedulerFactoryBean schedulerFactoryBean() throws IOException {
-        SchedulerFactoryBean factoryBean = new SchedulerFactoryBean();
-        factoryBean.setDataSource(dataSource);
-        factoryBean.setJobFactory(quartzJobFactory);
-        factoryBean.setQuartzProperties(quartzProperties());
-        factoryBean.setOverwriteExistingJobs(true);
-        factoryBean.setStartupDelay(10);
-        return factoryBean;
+    public SchedulerFactoryBean schedulerFactoryBean(CustomJobFactory customJobFactory) throws IOException {
+        SchedulerFactoryBean schedulerFactoryBean = new SchedulerFactoryBean();
+        schedulerFactoryBean.setQuartzProperties(quartzProperties());
+        schedulerFactoryBean.setJobFactory(customJobFactory);
+        return schedulerFactoryBean;
     }
 
     @Bean
@@ -57,21 +43,7 @@ public class QuartzConfig {
     }
 
     @Bean
-    public Scheduler scheduler() throws IOException {
-        return schedulerFactoryBean().getScheduler();
-    }
-
-    @Component
-    public static class QuartzJobFactory extends AdaptableJobFactory {
-
-        @Autowired
-        private AutowireCapableBeanFactory beanFactory;
-
-        @Override
-        protected Object createJobInstance(TriggerFiredBundle bundle) throws Exception {
-            Object jobInstance = super.createJobInstance(bundle);
-            beanFactory.autowireBean(bundle);
-            return jobInstance;
-        }
+    public Scheduler scheduler(SchedulerFactoryBean schedulerFactoryBean) throws IOException {
+        return schedulerFactoryBean.getScheduler();
     }
 }
