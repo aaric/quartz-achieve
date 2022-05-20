8ae461d3fd8f8adb6417fc6a401d80f67ae91ccd
qlhan <qlhan@incarcloud.com>
2022-05-20 14:29:44 +0800

    0.3.0-SNAPSHOT testTriple

--- quartz-simple/src/test/java/com/github/aaric/quartz/quartz/job/TupleTests.java
+++ quartz-simple/src/test/java/com/github/aaric/quartz/quartz/job/TupleTests.java
@@ -2,7 +2,9 @@ package com.github.aaric.quartz.quartz.job;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.ImmutableTriple;
 import org.apache.commons.lang3.tuple.Pair;
+import org.apache.commons.lang3.tuple.Triple;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -17,15 +19,27 @@ import java.time.LocalDate;
 @Slf4j
 public class TupleTests {
 
-    private static Pair<Integer, Integer> yearMonth(LocalDate date) {
+    private static Pair<Integer, Integer> yyMM(LocalDate date) {
         return ImmutablePair.of(date.getYear(), date.getMonthValue());
     }
 
     @Test
     public void testPair() {
         Assertions.assertDoesNotThrow(() -> {
-            Pair<Integer, Integer> yearMonth = yearMonth(LocalDate.now());
-            log.info("{}-{}", yearMonth.getLeft(), yearMonth.getRight());
+            Pair<Integer, Integer> yyMM = yyMM(LocalDate.now());
+            log.info("{}-{}", yyMM.getLeft(), yyMM.getRight());
+        });
+    }
+
+    private Triple<Integer, Integer, Integer> yyMMdd(LocalDate date) {
+        return ImmutableTriple.of(date.getYear(), date.getMonthValue(), date.getDayOfMonth());
+    }
+
+    @Test
+    public void testTriple() {
+        Assertions.assertDoesNotThrow(() -> {
+            Triple<Integer, Integer, Integer> yyMMdd = yyMMdd(LocalDate.now());
+            log.info("{}-{}-{}", yyMMdd.getLeft(), yyMMdd.getMiddle(), yyMMdd.getRight());
         });
     }
 }
