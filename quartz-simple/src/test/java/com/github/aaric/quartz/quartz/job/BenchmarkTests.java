7a48751ba767cdc890cca98285d56898a3c10d76
qlhan <qlhan@incarcloud.com>
2022-05-27 10:53:09 +0800

    0.3.0-SNAPSHOT test jmh

---
+++ quartz-simple/src/test/java/com/github/aaric/quartz/quartz/job/BenchmarkTests.java
@@ -0,0 +1,24 @@
+package com.github.aaric.quartz.quartz.job;
+
+/**
+ * BenchmarkTests
+ *
+ * @author Aaric, created on 2022-05-27T10:44.
+ * @version 0.3.0-SNAPSHOT
+ */
+//@Measurement(iterations = 20, time = 1, batchSize = 1000)
+//public class BenchmarkTests {
+//
+//    @Benchmark
+//    public void testIntStream() {
+//        IntStream.range(1, 1000).boxed().collect(Collectors.toList());
+//    }
+//
+//    @Test
+//    public void testBench() throws Exception {
+//        Options options = new OptionsBuilder()
+//                .include(BenchmarkTests.class.getSimpleName())
+//                .build();
+//        new Runner(options).run();
+//    }
+//}
